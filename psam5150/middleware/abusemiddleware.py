a44276be63b715843289c8001ec22bb63f2ef7a7
Levi Gross <levi@levigross.com>
2012-03-29 17:00:32 -0400

    Add example middleware

--- psam5150/middleware/abusemiddleware.py
+++ psam5150/middleware/abusemiddleware.py
@@ -1,1 +1,10 @@
 # -*- coding: UTF-8 -*-
+
+class AbuseMiddleware(object):
+
+    def process_request(self, request):
+        print "I am a request"
+
+    def process_response(self, request, response):
+        print "I am a response"
+        return response
--- psam5150/settings.py
+++ psam5150/settings.py
@@ -98,6 +98,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'middleware.abusemiddleware.AbuseMiddleware',
     )
 
 AUTH_PROFILE_MODULE = 'messageboard.'
